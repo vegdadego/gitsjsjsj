8b5889fd7c393a04b0847678a9aeda06ace1bc7d
vegdadego <jfytrgg@gmail.com>
2025-02-25 12:48:12 -0600

    Add files via upload
    examen parcial 2

---
+++ tienda/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Producto 
+{
+    public string Nombre { get; set;}
+    public double Precio {get; set;}
+    public int Codigo {get; set;}
+
+    public Producto(string nombre, double precio, int codigo)
+
+    {
+        Nombre = nombre;
+        Precio = precio;
+        Codigo = codigo;
+    }
+
+    public override string ToString()
+    {
+        return $"Nombre: {Nombre}, Precio: {Precio}, codigo: {Precio:C}";
+    }
+
+    class Tienda
+    {
+        private Producto Producto1;
+        private Producto Producto2;
+        private Producto Producto3;
+    }
+
+    public Tienda(Producto producto1, Producto producto2, Producto producto3)
+    {
+        producto1 = producto1;
+        producto2 = producto2;
+        producto3 = producto3;
+    }
+
+
+
+}
